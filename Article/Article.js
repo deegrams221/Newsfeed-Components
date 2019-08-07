7d82d52d1fd6a48e2cf0446812b5d636fe49f9bb
Diana Grams <deegrams221@gmail.com>
2019-08-07 14:06:46 -0600

    created function and return

--- Article/Article.js
+++ Article/Article.js
@@ -103,12 +103,21 @@ const data = [
 
   Your function should take either an object as it's one argument, or 5 separate arguments mapping to each peice of the data object above.
 
-  Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
+ Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
 
-  Step 3: return the entire component.
+ Step 3: return the entire component.
 
-  Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
+ Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
 
-  Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new artible
-
+ Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article
 */
+
+
+// function
+function createArticle(data) {
+
+
+
+  // return
+  return articleDiv;
+}
